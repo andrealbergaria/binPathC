d85ba178a9754ab73f3367ca950375e13c497bc0
andre albergaria coelho <andre1coelho@outlook.com>
2021-03-10 16:53:17 +0000

    testing large numbers asm

--- original_wikipedia_c_implementation/files/createFileECB.sh
+++ original_wikipedia_c_implementation/files/createFileECB.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 seq 1 8 | tr '\n' 'A' > plainTextECB
-#seq 1 8 | tr '\n' 'A' >> plainTextECB
+seq 1 8 | tr '\n' 'B' >> plainTextECB
+seq 1 8 | tr '\n' 'C' >> plainTextECB
+seq 1 8 | tr '\n' 'D' >> plainTextECB
+
 
 ls -l plainText*
--- original_wikipedia_c_implementation/testNumbBytes/testLargeNumbers.asm
+++ original_wikipedia_c_implementation/testNumbBytes/testLargeNumbers.asm
@@ -8,27 +8,59 @@
 
 section .data
 	
-    	key:     dd 0 ; use limits in one index, increment nextIndex, limits again
-	buf:  dd 0
-
-	fmt: db 0xa,'Buf %x',0xa
+    key:     db 0,0
 	
-
+	fmt: db 0xa,'Buf %x , IDX : %x',0xa
+		
+;nasm testLargeNumbers.asm -g -F dwarf -f elf32 -o tryCombs.o
 section .text
-
+; ebx points to current byte of key (the array, meaning its a index), assigning it values
+; esi has length in bytes of key
 
 main:
+	xor ebx,ebx
 	
-	mov ebx, dword [key]
-	mov dword [buf],ebx
-	push dword [buf]
-	push fmt
-	call printf
+	; number of iterations on increment one byte (0xff)
+	; bytes to cycle ( ebx =0 , 0xff , ebx=1 , 0xffff )
 
-	add dword  [key],1
+; specify byte and increment from 0 to 0xff. Byte number is on ebx
+	; ebx -> Number of bytes to check
+	incrementFF:
+		cmp ebx,2
+		je done
+		
+		cmp DWORD [key+ebx], 0xff
+		je incrementInterval
 
-	cmp dword [key],5
-	je end
-	jmp main
+afterIncrementInterval:
+		add DWORD [key+ebx],1
+		
+		push ebx 
+		push dword [key+ebx] ; needs to mulitply by 4 because im only testing byte to byte (4 because the real func is int)
+		push fmt
+		call printf
+	
+	
+		
+		jmp incrementFF
+			
+incrementInterval:
+		inc ebx
+		jmp afterIncrementInterval
 
-end:
+		
+		
+
+;	mov edx,0	;beggining of array
+;	mov ecx,1   ;end of array (number of bytes required, for three 0xff , ecx=2)
+	
+
+	
+;	mov edx,0  ; repeats last component of array [255....255...0] to set up the numbers
+	;inc ecx 
+;	call incrementFF
+	
+done:
+ 	mov eax, 1          ;system call number (sys_exit)
+    int 0x80            ;call kernel
+	
